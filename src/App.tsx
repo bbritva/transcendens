5de06ab205d1f2b86d7583b8280f92af6254cb22
Iaroslav NEVEROV <beeguy74@gmail.com>
2022-12-06 15:43:25 +0100

    so Auth is works

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import { Authorization } from './features/authorization/Authorization';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+	<Authorization />
         <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
