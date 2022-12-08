54ce135bdf1f30bfbfeb599bc0b2f48d04f31cba
Iaroslav NEVEROV <beeguy74@gmail.com>
2022-12-08 21:33:16 +0100

    react works,

--- front/src/app/store.ts
+++ front/src/app/store.ts
@@ -1,5 +1,5 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import authorizationReducer from 'src/features/authorization/authorizationSlice';
+import authorizationReducer from 'src/store/authorizationSlice';
 
 export const store = configureStore({
   reducer: {
