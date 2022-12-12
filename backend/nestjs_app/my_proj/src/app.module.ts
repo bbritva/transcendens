a1c39b1442cff50afa9dd2120cdd3cdb6fe26d26
grvelva <grvelva@student.42.fr>
2022-12-12 13:29:43 +0100

    fix after reverting merge =D

--- backend/nestjs_app/my_proj/src/app.module.ts
+++ backend/nestjs_app/my_proj/src/app.module.ts
@@ -3,9 +3,7 @@ import { AppController } from './app.controller';
 
 //services
 import { AppService } from './app.service';
-import { AuthController } from './auth/auth.controller';
 import { HttpModule } from '@nestjs/axios';
-import { ReqService } from './req/req.service';
 import { GameModule } from './game/game.module';
 import { UserModule } from './user/user.module';
 
@@ -13,9 +11,8 @@ import { UserModule } from './user/user.module';
 @Module({
   imports: [HttpModule, GameModule, UserModule],
   controllers: [
-    AuthController,
     AppController,
   ],
-  providers: [AppService, ReqService],
+  providers: [AppService],
 })
 export class AppModule {}
