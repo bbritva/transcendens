e4f2736852c7443bec2e3268c3383909d853467f
Iaroslav NEVEROV <beeguy74@gmail.com>
2022-12-12 15:52:37 +0100

    installed font and resize logo

--- front/src/components/Navbar/Navbar.tsx
+++ front/src/components/Navbar/Navbar.tsx
@@ -3,34 +3,43 @@ import { useState } from 'react';
 import { Box, Grid, Link,  } from '@mui/material';
 import { routes } from 'src/routes';
 import { NavLink } from 'react-router-dom';
-import styled from '@emotion/styled';
-import { GridLogo } from '../Logo/Logo';
+import { GridLogo } from '../Logo/GridLogo';
 
 
 
 function Navbar() {
   const [anchorNav, setAnchorNav] = useState(null);
+  const myHeight = 100;
 
   return (
-    <Grid container item xs={12} justifyContent={'flex-start'} sx={{
-      backgroundColor: 'transparent'
+    <Grid container item xs={12} justifyContent={'flex-start'} 
+      sx={{
+      background:  'rgba(0, 0, 0, 0.4)',
+      height: myHeight
+      // backgroundColor: 'black',
+      // opacity: 0.4,
     }}>
-        <GridLogo />
-        {routes.map((page) => (
-          <Grid item spacing={4}>
-            <Link
-              key={page.key}
-              component={NavLink}
-              to={page.path}
-              color="white"
-              underline="hover"
-              variant="button"
-              sx={{ fontSize: "large", marginLeft: "2rem" }}
-            >
-              {page.title}
-            </Link>
-          </Grid>
-        ))}
+      <Grid item xs={2}/>{/* OFSET */}
+      <GridLogo size={ myHeight }></GridLogo>
+      {routes.map((page) => (
+        <Grid item spacing={4}
+          display={'flex'}
+          alignItems={'center'}
+          sx={{ display: { xs: "none", sm: "flex" } }}
+        >
+          <Link
+            key={page.key}
+            component={NavLink}
+            to={page.path}
+            color="white"
+            underline="hover"
+            variant="button"
+            sx={{ fontSize: "large", marginLeft: "2rem" }}
+          >
+            {page.title}
+          </Link>
+        </Grid>
+      ))}
   </Grid>
 );
 }
