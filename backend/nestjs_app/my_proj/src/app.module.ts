0d89d2aaac5f55b4eb3fabd363c563c0e7820a54
grvelva <grvelva@student.42.fr>
2022-12-15 13:54:47 +0100

    fix after merge

--- backend/nestjs_app/my_proj/src/app.module.ts
+++ backend/nestjs_app/my_proj/src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
-
+import { GameModule } from './game/game.module';
+import { UserModule } from './user/user.module';
 import { AppController } from './app.controller';
 
 //services
-import { AppService } from './app.service';
 import { AuthController } from './auth/auth.controller';
-import { HttpModule } from '@nestjs/axios';
 import { ReqService } from './req/req.service';
-import { GameModule } from './game/game.module';
-import { UserModule } from './user/user.module';
+
 import { AppService } from './app.service';
 import { ChatGateway } from './chat/chat.gateway';
 import { ChatModule } from './chat2/chat2.module';
