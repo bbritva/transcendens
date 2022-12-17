f2f7e0d094c69211bc6d4e590ff002a6526dc110
grvelva <grvelva@student.42.fr>
2022-12-17 14:40:36 +0100

    removed temporary files

--- backend/nestjs_app/my_proj/src/app.module.ts
+++ backend/nestjs_app/my_proj/src/app.module.ts
@@ -9,17 +9,12 @@ import { AuthController } from './auth/auth.controller';
 import { ReqService } from './req/req.service';
 
 import { AppService } from './app.service';
-import { ChatGateway } from './chat/chat.gateway';
 import { ChatModule } from './chat2/chat2.module';
 import { PrismaService } from './prisma.service';
 import { UserService } from './user/user.service';
 import { ChannelModule } from './channel/channel.module';
 import { AuthModule } from './auth/auth.module';
 import { TokenService } from './token/token.service';
-import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './auth/constants';
-import { JwtStrategy } from './auth/jwt.strategy';
 
 // puts warnings to console
 process.on('warning', (warning) => {
@@ -31,14 +26,10 @@ process.on('warning', (warning) => {
     HttpModule, 
     GameModule, 
     UserModule, 
-    AuthModule, 
-    // PassportModule, 
-    // JwtModule.register({
-    //   secret: jwtConstants.secret,
-    //   signOptions: { expiresIn: '60s' },
-    // })
+    AuthModule,
+    ChatModule,
+    ChannelModule,
   ],
-  imports: [HttpModule, GameModule, UserModule, ChatModule, ChannelModule],
   controllers: [
     AuthController,
     AppController,
@@ -49,7 +40,6 @@ process.on('warning', (warning) => {
     PrismaService, 
     UserService, 
     TokenService,
-    // JwtStrategy
   ],
 })
 export class AppModule {}
