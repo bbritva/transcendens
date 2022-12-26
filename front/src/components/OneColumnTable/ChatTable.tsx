0f2c262749a72bb7c10eadc151bef1d3025c70ae
Iaroslav NEVEROV <beeguy74@gmail.com>
2022-12-26 21:06:59 +0100

    initial sockets  and messageInterface on front

--- front/src/components/OneColumnTable/ChatTable.tsx
+++ front/src/components/OneColumnTable/ChatTable.tsx
@@ -1,6 +1,23 @@
 import { IconButton, InputAdornment, OutlinedInput, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, useTheme } from "@mui/material";
-import { ReactElement, FC, useState, useRef } from "react";
+import { ReactElement, FC, useState, useRef, useEffect } from "react";
+import { createContext } from 'react';
+import { useStore } from "react-redux";
+import { io, Socket } from 'socket.io-client';
+import { RootState } from 'src/store/store'
 
+
+export const socket = io('http://localhost:3000');
+export const WebsocketContext = createContext<Socket>(socket);
+
+interface messageI {
+  header: {
+    JWTtoken: string,
+    useerName: string,
+    sentAt: Date,
+    channel: string,
+  },
+  text: string
+}
 
 const ChatTable: FC<{
   name: string,
@@ -9,15 +26,47 @@ const ChatTable: FC<{
   getName: boolean,
 }> = ({ name, loading, elements, getName = true }): ReactElement => {
   const [value, setValue] = useState('');
+  const [messages, setMessages] = useState<messageI[]>([]);
   const theme = useTheme();
   const tableRef = useRef(null);
+  const { getState } = useStore();
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('Connected!');
+    });
+    socket.on('onMessage', (newMessage: messageI) => {
+      console.log('onMessage event received!');
+      console.log(newMessage);
+      setMessages((prev) => [...prev, newMessage]);
+    });
+    return () => {
+      console.log('Unregistering Events...');
+      socket.off('connect');
+      socket.off('onMessage');
+    };
+  }, []);
   const scroll = tableRef?.current;
   if (scroll && !loading) {
     // @ts-ignore
     scroll.scrollTop = scroll.scrollHeight;
   }
   const onSubmit = () => {
-    // socket.emit('newMessage', value);
+    const { user, auth } = getState() as RootState;
+    console.log(auth);
+    socket.emit(
+      'newMessage',
+      {
+        header: {
+          // @ts-ignore
+          // JWTtoken: auth.accessToken.access_token,
+          JWTtoken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NzM3MjUsInVzZXJuYW1lIjoidHBodW5nIiwiaWF0IjoxNjcyMDYwMDY2LCJleHAiOjE2NzIwNjAwNzZ9.Oc3ZGIdZutyOyIo1h2t0cMZZ5MbBnOuAhg23_z1OVjA',
+          userName: user.user?.name,
+          sentAt: new Date(),
+          channel: '',
+        },
+        text: value
+      }
+    );
     console.log('newMessage', value);
     setValue('');
   };
@@ -55,9 +104,10 @@ const ChatTable: FC<{
           {
             loading
               ? 'LOADING'
-              : elements.map((data) => {
+              : messages.map((data) => {
                 return (
                   <TableRow
+                    key={ crypto.randomUUID() }
                     sx={{
                     }}
                   >
@@ -67,13 +117,7 @@ const ChatTable: FC<{
                       sx={{
                       }}
                     >
-                      {
-                        getName
-                          ? data.name
-                          : data.name == 'Chat'
-                            ? data.name + ' was created by ' + data.model
-                            : data.model
-                      }
+                      {data.text}
                     </TableCell>
                   </TableRow>);
               })
