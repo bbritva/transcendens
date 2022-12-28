ab5d2acafb65a8d114b6da9dbd8307866914480c
Iaroslav NEVEROV <beeguy74@gmail.com>
2022-12-28 20:03:42 +0100

    list elements in table now buttons

--- front/src/components/OneColumnTable/OneColumnTable.tsx
+++ front/src/components/OneColumnTable/OneColumnTable.tsx
@@ -1,67 +1,78 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, useTheme } from "@mui/material";
+import { Button, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography, useTheme } from "@mui/material";
 import { ReactElement, FC, useRef, CSSProperties } from "react";
 import { chatStylesI } from "src/pages/Chat/ChatPage";
 
 
 const anchorStyle = {
-  overflowAnchor:'auto',
-  height:'1px'
+  overflowAnchor: 'auto',
+  height: '1px'
 };
 
 const OneColumnTable: FC<{
-    name: string,
-    loading: boolean,
-    elements: [{name: string, model: string}],
-    getName: boolean,
-    chatStyles: chatStylesI
-  }> = ({name, loading, elements, getName = true, chatStyles}): ReactElement => {
-  const theme = useTheme();
-  const tableRef = useRef(null);
-  return (
-    <TableContainer
-        ref={tableRef}
+  name: string,
+  loading: boolean,
+  elements: [{ name: string, model: string }],
+  chatStyles: chatStylesI,
+  selectedElement: {},
+  setElement: ({ }) => void
+}> = ({
+  name,
+  loading,
+  elements,
+  chatStyles,
+  selectedElement,
+  setElement
+}): ReactElement => {
+    const theme = useTheme();
+    const tableRef = useRef(null);
+    return (
+      <Grid container
         component={Paper}
         sx={{
-          height: '99.3%',
+          display: "flex",
+          justifyContent: "center",
+          height: '100%',
           ...chatStyles.borderStyle,
-          ...chatStyles.scrollStyle
-        }}
-      >
-        <Table
+        }}>
+        <Typography variant="h6" maxHeight='3rem'
           sx={{
-            tableLayout: "auto",
-            width: "max-content"
+            ...chatStyles.textElipsis
           }}
         >
-          <TableHead>{name}</TableHead>
-          <TableBody
-          >
-            {
-              loading
-                ? 'LOADING'
-                : elements.map((data) => {
-                  return (
-                    <TableRow
-                      key={crypto.randomUUID()}
-                    >
-                      <TableCell>
-                        {
-                          getName
-                          ? data.name
-                          : data.name == 'Chat'
-                            ? data.name + ' was created by ' + data.model
-                            : data.model
-                        }
-                      </TableCell>
-                    </TableRow>);
-                })
-            }
-            <div style={anchorStyle as CSSProperties}></div>
-          </TableBody>
-        </Table>
-      </TableContainer>
-  );
-}
+          {name}
+        </Typography>
+        <Grid
+          ref={tableRef}
+          sx={{
+            height: '90%',
+            ...chatStyles.scrollStyle
+          }}>
+          {
+            loading
+              ? 'LOADING'
+              : elements.map((data) => {
+                return (
+                  <Button
+                    key={data.name}
+                    variant={selectedElement == data ? 'contained' : 'text'}
+                    onClick={() => { setElement(data) }}
+                    size='small'
+                    sx={{
+                      textAlign: 'left',
+                      maxHeight: '2rem',
+                      ...chatStyles.textElipsis
+                    }}
+                  >
+                    {data.name}
+                  </Button>
+                )
+              })
+          }
+          <div style={anchorStyle as CSSProperties} />
+        </Grid>
+      </Grid>
+    );
+  }
 
 
 export default OneColumnTable;
