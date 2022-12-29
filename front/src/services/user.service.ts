9f3b737fb26cf4e74b007829e0b05bb4cb0d5740
Iaroslav NEVEROV <beeguy74@gmail.com>
2022-12-29 13:31:34 +0100

    merge trash was deleted

--- front/src/services/user.service.ts
+++ front/src/services/user.service.ts
@@ -4,20 +4,15 @@ import authHeader from './authHeader';
 const API_URL = process.env.REACT_APP_USERS_URL;
 
 class UserService {
-<<<<<<< HEAD
-=======
-  getUsers() {
-    return axios.get("https://swapi.dev/api" + '/starships');
-  }
 
->>>>>>> 0ef4b54 (littl bugs)
   getMe() {
     const storageData = localStorage.getItem('access_token') || '{}';
     const token = JSON.parse(storageData);
     return axios.get(API_URL + '/getMe')
   }
+
   getUsers() {
-    return axios.get(API_URL + '/starships');
+    return axios.get("https://swapi.dev/api" + '/starships');
   }
 
   getPublicContent() {
