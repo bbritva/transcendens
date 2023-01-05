5bcf022afca98e9665d198eff8f3b601231597ab
grvelva <grvelva@student.42.fr>
2023-01-05 18:38:00 +0100

    added saving users

--- backend/src/chat/websocket/gateway.ts
+++ backend/src/chat/websocket/gateway.ts
@@ -1,4 +1,4 @@
-import { Body, OnModuleInit } from "@nestjs/common";
+import { OnModuleDestroy, OnModuleInit } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import {
   ConnectedSocket,
@@ -8,20 +8,20 @@ import {
   WebSocketServer,
 } from "@nestjs/websockets";
 import { Server, Socket } from "socket.io";
-import { CreateMessageDTO, PrivateMessageDTO } from "src/chat/message/dto/create-message.dto";
+import {
+  CreateMessageDTO,
+  PrivateMessageDTO,
+} from "src/chat/message/dto/create-message.dto";
 import { MessageService } from "src/chat/message/message.service";
 import { ClientDTO } from "./client.dto";
 import { ConnectedClientInfo } from "./connectedClientInfo";
 import { DecodedTokenDTO } from "./decodedToken.dto";
-import { UserService } from 'src/user/user.service';
-import { isError } from '@jest/expect-utils';
+import { UserService } from "src/user/user.service";
 
-type Connections = {
-  [key: string]: ConnectedClientInfo;
-};
+type Connections = Map<string, ConnectedClientInfo>;
 
-interface newSocket extends Socket{
-  username: string
+interface newSocket extends Socket {
+  username: string;
 }
 
 @WebSocketGateway({
@@ -34,7 +34,7 @@ export class Gateway implements OnModuleInit {
     private messageService: MessageService,
     private jwtService: JwtService,
     private userService: UserService,
-    private connections: Connections = {}
+    private connections: Connections = new Map()
   ) {}
 
   @WebSocketServer()
@@ -51,46 +51,60 @@ export class Gateway implements OnModuleInit {
       socket.username = username;
       next();
     });
-    if (true){
+    if (true) {
       this.server.on("connection", async (socket) => {
-        console.log('connected', socket.id);
-        console.log('connected', socket.handshake.auth.username);
-        this.connections[socket.id] = socket.handshake.auth.username;
-        const users = await this.userService.users({})
-        let resp = [];
-        for (let [id , socket ] of this.server.of("/").sockets) {
-          resp.push({
-            //@ts-ignore
-            username: socket.username,
-            userID: id,
-            conncected: true,
+        console.log("connected", socket.id);
+        console.log("connected", socket.handshake.auth.username);
+        this.connections.set(socket.id, {
+          username: socket.handshake.auth.username,
         });
 
-        }
+        socket.on("disconnect", async () => {
+          console.log("disconnected", socket.id);
+          this.connections.delete(socket.id)
+          socket.broadcast.emit("user connected", {
+            userID: socket.id,
+            username: this.connections.get(socket.id),
+          });
+        });
+        //send to new user info about connected users
+        let resp = [];
+        this.connections.forEach((value: ConnectedClientInfo, key: string) => {
+          resp.push({
+            username: value.username,
+            userID: key,
+            conncected: true,
+          });
+          console.log(key, value);
+        });
         socket.emit("users", resp);
-      });
-      this.server.on("connection", (socket: newSocket) => {
-        // notify existing users
+
+        //send to connected users info about new user
         socket.broadcast.emit("user connected", {
           userID: socket.id,
-          username: socket.username,
+          username: this.connections.get(socket.id),
         });
       });
-    }
-    else this.server.close();
+    } else this.server.close();
   }
 
-  @SubscribeMessage('private message')
-  async onPrivateMessage(@ConnectedSocket() client: ClientDTO, @MessageBody() data: PrivateMessageDTO) {
-    console.log("private", data)
+  @SubscribeMessage("private message")
+  async onPrivateMessage(
+    @ConnectedSocket() client: ClientDTO,
+    @MessageBody() data: PrivateMessageDTO
+  ) {
+    console.log("private", data);
     this.server.to(data.to).emit("private message", {
       content: data.content,
       from: client.id,
     });
   }
 
-  @SubscribeMessage('newMessage')
-  async onNewMessage(@ConnectedSocket() client: ClientDTO, @MessageBody() data: CreateMessageDTO) {
+  @SubscribeMessage("newMessage")
+  async onNewMessage(
+    @ConnectedSocket() client: ClientDTO,
+    @MessageBody() data: CreateMessageDTO
+  ) {
     console.log(this.connections[client.id]);
     console.log(data);
     console.log(this.connections);
