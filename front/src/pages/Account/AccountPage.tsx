430a4e22915bf830b8ed980be58c798bf836a430
Iaroslav NEVEROV <beeguy74@gmail.com>
2023-01-07 14:21:17 +0100

    fetched users in  AccountPage

--- front/src/pages/Account/AccountPage.tsx
+++ front/src/pages/Account/AccountPage.tsx
@@ -1,18 +1,25 @@
-import {ReactElement, FC, useState} from "react";
+import {ReactElement, FC, useState, useEffect} from "react";
 import {Box, Grid, Typography} from "@mui/material";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector, useStore } from "react-redux";
 import SignUp from "src/components/AccountUpdate/AccountUpdate";
 import { selectLoggedIn } from "src/store/authReducer";
-import { selectUser } from "src/store/userSlice";
+import { getFriends, selectUser } from "src/store/userSlice";
 import OneColumnTable from "src/components/OneColumnTable/OneColumnTable";
 import { userFromBackI } from "src/pages/Chat/ChatPage";
 import { chatStyles } from "../Chat/chatStyles";
+import { AppDispatch } from "src/store/store";
 
 const AccountPage: FC<any> = (): ReactElement => {
   const isLoggedIn = useSelector(selectLoggedIn);
-  const {user, status, error} = useSelector(selectUser);
-  const [friends, setFriends] = useState<userFromBackI[]>([] as userFromBackI[]);
+  const {user, status, error, friendsStatus, friends} = useSelector(selectUser);
   const [friend, setFriend] = useState<userFromBackI>({} as userFromBackI);
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    if (user && friendsStatus === 'idle'){
+      dispatch(getFriends())
+    }
+  }, [user, friendsStatus]);
 
   return (
     <Box sx={{
@@ -28,7 +35,7 @@ const AccountPage: FC<any> = (): ReactElement => {
         :
         <>
           <Grid item xs={2}>
-            <OneColumnTable name="your friends" loading={true} elements={friends} chatStyles={chatStyles} selectedElement={friend} setElement={setFriend} dialogChildren={<></>} />
+            <OneColumnTable name="your friends" loading={friendsStatus != "succeeded"} elements={friends} chatStyles={chatStyles} selectedElement={friend} setElement={setFriend} dialogChildren={<></>} />
           </Grid>
           <Grid item xs={10}>
             <SignUp />
