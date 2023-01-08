ab020ae099205bfdaeeea1b11f6932f86763df4e
grvelva <grvelva@student.42.fr>
2023-01-08 16:19:45 +0100

    simplified message DTO

--- backend/src/chat/message/entities/message.entity.ts
+++ backend/src/chat/message/entities/message.entity.ts
@@ -4,12 +4,16 @@ import { ApiProperty } from "@nestjs/swagger";
 export class MessageEntity implements Message {
   @ApiProperty({ uniqueItems: true })
   id: number;
+
   @ApiProperty()
   channelName: string;
+  
   @ApiProperty()
   sentAt: Date;
+  
   @ApiProperty()
   authorName: string;
+  
   @ApiProperty()
   text: string;
 
