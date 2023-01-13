5c5abe86189c7fff8de49cc9fe35e9c125af9693
Iaroslav NEVEROV <beeguy74@gmail.com>
2023-01-13 11:51:05 +0100

    button to start the game

--- front/src/pages/Game/GamePage.tsx
+++ front/src/pages/Game/GamePage.tsx
@@ -1,5 +1,5 @@
 import { ReactElement, FC, useRef, useEffect, useState } from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import Canvas, { canvasPropsI } from "./components/Canvas";
 import game from "./components/game";
 
@@ -8,15 +8,21 @@ const GamePage: FC<any> = (): ReactElement => {
   const [stopGame, setStopGame] = useState<boolean>();
 
   useEffect(() => {
-    const canvas = canvasRef.current
+    const canvas = canvasRef.current;
     if (canvas){
       setStopGame(false);
-      game(canvas, setStopGame);
       return () => {
         setStopGame(true);
       }
     }
   }, [])
+
+  function startGame(){
+    const canvas = canvasRef.current;
+    if (canvas){
+      game(canvas, setStopGame);
+    }
+  }
 
   const canvasProps = {
     width: "720",
@@ -31,6 +37,7 @@ const GamePage: FC<any> = (): ReactElement => {
       alignItems: 'center',
     }}>
       <Typography variant="h3">GamePage</Typography>
+      <Button children={'StartGame'} variant={'outlined'} size="large" onClick={startGame}/>
       <Canvas ref={canvasRef} {...canvasProps} />
     </Box>
   );
