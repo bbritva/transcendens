554e18f219d8f9f02091ee852fb7160ed924538e
ddiakova <ddiakova@student.42.fr>
2023-02-12 21:02:57 +0100

    styled method - NavButton application

--- front/src/components/Navbar/Navbar.tsx
+++ front/src/components/Navbar/Navbar.tsx
@@ -1,75 +1,74 @@
 import { useSelector } from "react-redux";
-import { NavLink } from 'react-router-dom';
-import { Typography, AppBar, Box, Grid, Button, alpha, useTheme } from '@mui/material';
-import { routes } from 'src/routes';
-import { GridLogo } from 'src/components/Logo/GridLogo';
-import { selectUser } from 'src/store/userSlice';
+import { NavLink } from "react-router-dom";
+import {
+  Typography,
+  AppBar,
+  Box,
+  Grid,
+  Button,
+  alpha,
+  useTheme,
+} from "@mui/material";
+import { routes } from "src/routes";
+import { GridLogo } from "src/components/Logo/GridLogo";
+import { selectUser } from "src/store/userSlice";
 import Protected from "../Authentication/Protected";
 import { Container } from "@mui/system";
-
+import { StyledNavButton } from "../NavButton/StyledNavButton";
+import NavButton from "../NavButton/NavButton";
 
 interface NavbarProps {
-  loginButtonText: string,
-  onLoginClick: () => void,
-  onLogoutClick: () => void
+  loginButtonText: string;
+  onLoginClick: () => void;
+  onLogoutClick: () => void;
 }
 
-function Navbar({ loginButtonText, onLoginClick, onLogoutClick}: NavbarProps) {
+function Navbar({ loginButtonText, onLoginClick, onLogoutClick }: NavbarProps) {
   // const [anchorNav, setAnchorNav] = useState(null); //will use it for menu
   const { user, status, error } = useSelector(selectUser);
   const theme = useTheme();
-  const myHeight = '10vh';
+  const myHeight = "10vh";
   const navButtonStyle = {
-    display: { xs: 'none', md: 'flex' },
-    fontSize: "large", marginLeft: "3rem",
-    '&:hover': {
+    display: { xs: "none", md: "flex" },
+    fontSize: "large",
+    marginLeft: "3rem",
+    "&:hover": {
       backgroundColor: theme.palette.primary.light,
-    }
+    },
   } as const;
   return (
     <AppBar position="sticky">
-        <Container>
-      <Box sx={{ flexGrow: 1, display: { xs: 'flex' } }}>
-      <GridLogo size={100}></GridLogo>
-      {routes.map((page) => (
-          <Button
-            key={page.key}
-            component={NavLink}
-            to={page.path}
-            variant={'text'}
-            sx={navButtonStyle}
-          >
-            <Typography  color="secondary">
+      <Container>
+        <Box sx={{ flexGrow: 1, display: { xs: "flex" } }}>
+          <GridLogo size={100}></GridLogo>
+          {routes.map((page) => (
+            <NavButton page={page}>
               {page.title == "Account" && user ? user.name : page.title}
-            </Typography>
-          </Button>
-
-      ))}{
-        <Protected
-          user={user}
-          render={() =>
-            <Button
-              sx={navButtonStyle}
-              variant={'outlined'}
-              onClick={onLogoutClick}
-            >
-              Logout
-            </Button>
-          }
-          fail={() =>
-            <Button
-              sx={navButtonStyle}
-              variant={'contained'}
-              onClick={onLoginClick}
-            >
-              {loginButtonText}
-            </Button>
-          }
-        />
-        }
-
-
-</Box>
+            </NavButton>
+          ))}
+          <Protected
+            user={user}
+            render={() => (
+              //TODO: MENU instead of logout
+              <Button
+                sx={navButtonStyle}
+                variant={"text"}
+                onClick={onLogoutClick}
+              >
+                <Typography color="secondary">Logout</Typography>
+              </Button>
+            )}
+            fail={() => (
+              <Button
+                sx={navButtonStyle}
+                variant={"text"}
+                onClick={onLoginClick}
+              >
+                <Typography color="secondary">{loginButtonText}</Typography>
+              </Button>
+            )}
+          />
+        </Box>
       </Container>
     </AppBar>
   );
