98da38e709e18a282e7d4cd4605e4d182610a9c0
Iaroslav NEVEROV <beeguy74@gmail.com>
2023-02-15 21:31:25 +0100

    Protected component's testing with mocke sessionStor

--- front/src/components/__tests__/Protected.test.js
+++ front/src/components/__tests__/Protected.test.js
@@ -79,8 +79,63 @@ afterEach(() => {
 
   });
 
-  it("should render fail login without user", () => {
+  it("should render success login with user and with mocked username", () => {
+    window.sessionStorage.setItem('username', 'vasya')
+    let user = {
+      id: '1',
+      name: 'vasya',
+      image: 'no-image',
+      avatar: '',
+      isTwoFaEnabled: false
+    };
 
+    act(() => {
+      root.render(
+        <Protected
+          user={user}
+          render={() => <div data-testid="success"></div>}
+          fail={() =>
+            <div data-testid="fail"></div>
+          }
+        />
+      );
+    });
+
+    expect(
+      container.querySelector("[data-testid='success']")
+    ).toBeTruthy();
+
+    expect(
+      container.querySelector('[data-testid="fail"]')
+    ).toBeFalsy();
+
+  });
+
+  it("should render success login without user but with mocked storage username", () => {
+    window.sessionStorage.setItem('username', 'vasya')
+    act(() => {
+      root.render(
+        <Protected
+          user={null}
+          render={() => <div data-testid="success"></div>}
+          fail={() =>
+            <div data-testid="fail"></div>
+          }
+        />
+      );
+    });
+
+    expect(
+      container.querySelector("[data-testid='success']")
+    ).toBeTruthy();
+
+    expect(
+      container.querySelector('[data-testid="fail"]')
+    ).toBeFalsy();
+
+  });
+
+  it("should render fail login without user", () => {
     act(() => {
       root.render(
         <Protected
