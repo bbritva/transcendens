b114d4a82fc8bf0322f13440ff010bc72c0b6ff1
ddiakova <ddiakova@student.42.fr>
2023-02-22 22:36:24 +0100

    transition element component

---
+++ front/src/components/TransitionComponent/TransitionComponent.tsx
@@ -0,0 +1,59 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Switch from "@mui/material/Switch";
+import Paper from "@mui/material/Paper";
+import Slide from "@mui/material/Slide";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import { Theme } from "@mui/material/styles";
+
+export interface transitionComponentI {
+  children: React.ReactElement;
+  title: string;
+}
+const icon = (
+    <Paper sx={{ m: 1 }} elevation={4}>
+      <Box component="svg" sx={{ width: 100, height: 100 }}>
+        <Box
+          component="polygon"
+          sx={{
+            fill: (theme: Theme) => theme.palette.common.white,
+            stroke: (theme) => theme.palette.divider,
+            strokeWidth: 1,
+          }}
+          points="0,100 50,00, 100,100"
+        />
+      </Box>
+    </Paper>
+  );
+
+const TransitionComponent: React.FC<transitionComponentI> = ({
+  children,
+  title,
+}) => {
+  const [checked, setChecked] = React.useState(false);
+
+  const handleChange = () => {
+    setChecked((prev) => !prev);
+  };
+
+  return (
+    <Box>
+      <Box sx={{ width: `calc(100px + 16px)` }}>
+        <FormControlLabel
+          control={<Switch checked={checked} onChange={handleChange} />}
+          label={title}
+        />
+       
+          <Slide direction="up" in={checked} mountOnEnter unmountOnExit>
+            {
+              icon
+              //children
+            }
+          </Slide>
+        
+      </Box>
+    </Box>
+  );
+};
+
+export default TransitionComponent;
