46ad302647d0af7e0cbc209ff85d06d08a43ef60
ddiakova <ddiakova@student.42.fr>
2023-03-08 16:38:34 +0100

    some typo modifications on chat page

--- front/src/components/OneColumnTable/OneColumnTable.tsx
+++ front/src/components/OneColumnTable/OneColumnTable.tsx
@@ -1,28 +1,41 @@
-import { Button, CardMedia, Grid, Paper, Typography, useTheme } from "@mui/material";
-import { ReactElement, FC, useRef, CSSProperties, useState, ReactNode, cloneElement, Children } from "react";
+import {
+  CardMedia,
+  Grid,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import {
+  ReactElement,
+  FC,
+  useRef,
+  CSSProperties,
+  useState,
+  ReactNode,
+  cloneElement,
+  Children,
+} from "react";
 import DialogSelect from "src/components/DialogSelect/DialogSelect";
 import { fromBackI } from "src/pages/Chat/ChatPage";
 import { chatStylesI } from "src/pages/Chat/chatStyles";
-import AdjustOutlinedIcon from '@mui/icons-material/AdjustOutlined';
+import AdjustOutlinedIcon from "@mui/icons-material/AdjustOutlined";
 import { userI } from "src/store/userSlice";
 import React from "react";
-
+import { StyledMenuButton } from "../NavButton/StyledNavButton";
 
 const anchorStyle = {
-  overflowAnchor: 'auto',
-  height: '1px'
+  overflowAnchor: "auto",
+  height: "1px",
 };
 
-
 const OneColumnTable: FC<{
-  taper: string,
-  user: userI | null,
-  loading: boolean,
-  elements: fromBackI[],
-  chatStyles: chatStylesI,
-  selectedElement: {},
-  setElement: Function,
-  dialogChildren: ReactNode,
+  taper: string;
+  user: userI | null;
+  loading: boolean;
+  elements: fromBackI[];
+  chatStyles: chatStylesI;
+  selectedElement: {};
+  setElement: Function;
+  dialogChildren: ReactNode;
 }> = ({
   taper,
   user,
@@ -33,90 +46,90 @@ const OneColumnTable: FC<{
   setElement,
   dialogChildren,
 }): ReactElement => {
-    const theme = useTheme();
-    const tableRef = useRef(null);
-    const [openDialog, setOpenDialog] = useState(false);
-    const child = Children.only(dialogChildren);
-    return (
-      <Grid container
-        component={CardMedia}
-        color={theme.palette.primary.dark}
+  const theme = useTheme();
+  const tableRef = useRef(null);
+  const [openDialog, setOpenDialog] = useState(false);
+  const child = Children.only(dialogChildren);
+  return (
+    <Grid
+      container
+      component={CardMedia}
+      color={theme.palette.primary.dark}
+      display="flex"
+      justifyContent="center"
+      sx={{
+        height: "100%",
+        ...chatStyles.borderStyle,
+        borderRadius: "0.5rem",
+        background: "linear-gradient(to top, #8bd4d1, 15%, #ecebd9)",
+      }}
+    >
+      <Grid item xs={12} display="inherit" justifyContent={"inherit"}>
+        <Typography
+          marginTop={"1rem"}
+          variant="body1"
+          maxHeight="3rem"
+          sx={{
+            ...chatStyles.textElipsis,
+          }}
+        >
+          {taper}
+        </Typography>
+      </Grid>
+      <Grid
+        item
+        ref={tableRef}
         display="flex"
-        justifyContent="center"
+        flexDirection={"column"}
         sx={{
-          height: '100%',
-          ...chatStyles.borderStyle,
-          // backgroundColor: theme.palette.secondary.main
-          background: "linear-gradient(to top, #8bd4d1, 15%, #ecebd9)",
+          height: "90%",
+          ...chatStyles.scrollStyle,
         }}
       >
-        <Grid
-          item
-          xs={12}
-          display="inherit"
-          justifyContent={'inherit'}
-        >
-          <Typography variant="h6" maxHeight='3rem'
-            sx={{
-              ...chatStyles.textElipsis
-            }}
-          >
-            {taper}
-          </Typography>
-        </Grid>
-        <Grid
-          item
-          ref={tableRef}
-          display="flex"
-          flexDirection={'column'}
-          sx={{
-            height: '90%',
-            ...chatStyles.scrollStyle
-          }}>
-          {
-            loading
-              ? 'LOADING'
-              : elements.map((data) => {
-                if (!(taper === 'Users' && user?.name === data.name))
-                  return (
-                    <Button
-                      key={data.name}
-                      variant={selectedElement == data ? 'contained' : 'text'}
-                      startIcon={data.connected && < AdjustOutlinedIcon fontSize="small" />}
-                      onClick={() => {
-                        setElement(data);
-                        setOpenDialog(true);
-                      }}
-                      size='small'
-                      sx={{
-                        textAlign: 'left',
-                        maxHeight: '2rem',
-                      }}
-                    >
-                      <Typography noWrap>
-                        {data.name}
-                      </Typography>
-                    </Button>
-                  )
-              })
-          }
-          <div style={anchorStyle as CSSProperties} />
-        </Grid>
-        <DialogSelect
-          options={selectedElement}
-          open={openDialog}
-          setOpen={setOpenDialog}
-        >
-          {
-            React.isValidElement(child)
-            //@ts-ignore
-            ? cloneElement(child, {setOpen: setOpenDialog})
-            : <></>
-          }
-        </DialogSelect>
+        {loading
+          ? "LOADING"
+          : elements.map((data) => {
+              if (!(taper === "Users" && user?.name === data.name))
+                return (
+                  <StyledMenuButton
+                    fullWidth
+                    key={data.name}
+                    variant={selectedElement == data ? "contained" : "text"}
+                    startIcon={
+                      data.connected && <AdjustOutlinedIcon fontSize="small" />
+                    }
+                    onClick={() => {
+                      setElement(data);
+                      setOpenDialog(true);
+                    }}
+                    size="small"
+                    sx={{
+                      textAlign: "left",
+                      maxHeight: "2rem",
+                    }}
+                  >
+                    <Typography variant="subtitle1" noWrap>
+                      {data.name}
+                    </Typography>
+                  </StyledMenuButton>
+                );
+            })}
+        <div style={anchorStyle as CSSProperties} />
       </Grid>
-    );
-  }
-
+      <DialogSelect
+        options={selectedElement}
+        open={openDialog}
+        setOpen={setOpenDialog}
+      >
+        {React.isValidElement(child) ? (
+          //@ts-ignore
+          cloneElement(child, { setOpen: setOpenDialog })
+        ) : (
+          <></>
+        )}
+      </DialogSelect>
+    </Grid>
+  );
+};
 
 export default OneColumnTable;
--- front/src/pages/Chat/ChatPage.tsx
+++ front/src/pages/Chat/ChatPage.tsx
@@ -1,8 +1,8 @@
 import { ReactElement, FC, useState, useEffect } from "react";
-import { Divider, Grid, Paper, useTheme  } from "@mui/material";
+import { Divider, useTheme } from "@mui/material";
 import OneColumnTable from "src/components/OneColumnTable/OneColumnTable";
 import ChatTable from "src/components/OneColumnTable/ChatTable";
-import { RootState } from 'src/store/store'
+import { RootState } from "src/store/store";
 import { useStore } from "react-redux";
 import ChatInput from "src/components/ChatInput/ChatInput";
 import ChooseDialogChildren from "src/components/DialogSelect/ChooseDialogChildren";
@@ -11,106 +11,111 @@ import socket from "src/services/socket";
 import { useAppDispatch } from "src/app/hooks";
 import { Box } from "@mui/system";
 
-
-export interface fromBackI{
-  name: string,
-  id: string,
-  hasNewMessages: boolean,
-  messages: newMessageI[],
-  connected: boolean,
-  data: any
+export interface fromBackI {
+  name: string;
+  id: string;
+  hasNewMessages: boolean;
+  messages: newMessageI[];
+  connected: boolean;
+  data: any;
 }
 
-export interface userFromBackI extends fromBackI{
-}
+export interface userFromBackI extends fromBackI {}
 
-export interface channelFromBackI extends fromBackI{
-  users: userFromBackI[],
-  hasPasswrod : boolean,
-  isPrivate : boolean
+export interface channelFromBackI extends fromBackI {
+  users: userFromBackI[];
+  hasPasswrod: boolean;
+  isPrivate: boolean;
 }
 
 export interface newMessageI {
-  id: number | null,
-  channelName: string,
-  sentAt: string | null,
-  authorName: string,
-  text: string,
+  id: number | null;
+  channelName: string;
+  sentAt: string | null;
+  authorName: string;
+  text: string;
 }
 
 export interface ChatPageProps {
-  channels : channelFromBackI[],
-  setChannels : Function
+  channels: channelFromBackI[];
+  setChannels: Function;
 }
 
 const ChatPage: FC<ChatPageProps> = ({
   channels,
-  setChannels
+  setChannels,
 }): ReactElement => {
   const [page, setPage] = useState(0);
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
   const [chosenChannel, setChosenChannel] = useState({} as channelFromBackI);
   const [loading, setLoading] = useState(false);
-  const [chosenUser, setChosenUser] = useState<userFromBackI>({} as userFromBackI);
-  const [destination, setDestination] = useState<[string, fromBackI]>(['', {} as fromBackI]);
+  const [chosenUser, setChosenUser] = useState<userFromBackI>(
+    {} as userFromBackI
+  );
+  const [destination, setDestination] = useState<[string, fromBackI]>([
+    "",
+    {} as fromBackI,
+  ]);
   const { getState } = useStore();
   const { user, auth } = getState() as RootState;
   const dispatch = useAppDispatch();
   const theme = useTheme();
-  const testUsername = sessionStorage.getItem('username');
+  const testUsername = sessionStorage.getItem("username");
 
   useEffect(() => {
     const [destTaper, destObject] = destination;
-    if (destTaper === 'Channels'){
+    if (destTaper === "Channels") {
       socket.emit(destObject.name, destObject.data);
       setChosenChannel(destObject as channelFromBackI);
-    }
-    else if (destTaper === 'Users'){
+    } else if (destTaper === "Users") {
       socket.emit(destObject.name, destObject.data);
-      setChosenChannel(destObject.data)
+      setChosenChannel(destObject.data);
     }
   }, [destination]);
 
-  chatStyles
-    .scrollStyle["&::-webkit-scrollbar-thumb"]
-    .backgroundColor = theme.palette.primary.light;
+  chatStyles.scrollStyle["&::-webkit-scrollbar-thumb"].backgroundColor =
+    theme.palette.primary.light;
 
   const onSubmit = () => {
     const [taper, destinationChannel] = destination;
-    if ( !destinationChannel.name ){
-      setValue('');
-      return ;
+    if (!destinationChannel.name) {
+      setValue("");
+      return;
     }
     const newMessage: newMessageI = {
       id: null,
       channelName: destinationChannel.name,
       sentAt: null,
-      authorName: testUsername || user.user?.name || '',
+      authorName: testUsername || user.user?.name || "",
       text: value,
     };
-    socket.emit(
-      'newMessage',
-      newMessage
-    );
-    setValue('');
+    socket.emit("newMessage", newMessage);
+    setValue("");
   };
 
   return (
-  <>
-      <Box flex={1} margin={'2rem'} sx={{ display: { xs: "none", sm: "none", md: "block"}}}>
+    <>
+      <Box
+        flex={1}
+        marginTop={"2rem"}
+        marginLeft={"2rem"}
+        marginRight={"1rem"}
+        marginBottom={"2rem"}
+        sx={{ display: { xs: "none", sm: "none", md: "block" } }}
+      >
         <OneColumnTable
-          taper='Channels'
+          taper="CHANNELS"
           user={user.user}
           loading={loading}
           elements={channels}
           chatStyles={chatStyles}
           selectedElement={chosenChannel}
           setElement={(channel: channelFromBackI) => {
-            setDestination(['Channels', channel]);
+            setDestination(["Channels", channel]);
           }}
           dialogChildren={
             <ChooseDialogChildren
-              dialogName='Channels'
+              dialogName="Channels"
               user={user.user}
               element={chosenChannel}
               channel={chosenChannel}
@@ -118,13 +123,16 @@ const ChatPage: FC<ChatPageProps> = ({
             />
           }
         />
-        </Box>
-      
-      <Box flex={4} height='70vh' margin={'2rem'}>
+      </Box>
+
+      <Box flex={4} height="70vh" marginTop={"2rem"}>
         <ChatTable
-          name={'Chat'}
+          name={"Chat"}
           loading={loading}
-          messages={channels.find((el) => el.name === chosenChannel.name)?.messages || []}
+          messages={
+            channels.find((el) => el.name === chosenChannel.name)?.messages ||
+            []
+          }
           socket={socket}
           chatStyles={chatStyles}
           user={user.user}
@@ -136,22 +144,31 @@ const ChatPage: FC<ChatPageProps> = ({
             value={value}
             setValue={setValue}
             onSubmit={onSubmit}
-        />
+          />
         </Box>
       </Box>
-      
-      <Box flex={1} margin={'2rem'} sx={{display: { xs: "none", sm: "block", md: "block"}}}>
+
+      <Box
+        flex={1}
+        marginTop={"2rem"}
+        marginLeft={"1rem"}
+        marginRight={"2rem"}
+        marginBottom={"2rem"}
+        sx={{ display: { xs: "none", sm: "block", md: "block" } }}
+      >
         <OneColumnTable
-          taper='Users'
+          taper="USERS"
           user={user.user}
           loading={loading}
-          elements={channels.find((el) => el.name === chosenChannel.name)?.users || []}
+          elements={
+            channels.find((el) => el.name === chosenChannel.name)?.users || []
+          }
           chatStyles={chatStyles}
           selectedElement={chosenUser}
           setElement={setChosenUser}
           dialogChildren={
             <ChooseDialogChildren
-              dialogName='Users'
+              dialogName="Users"
               user={user.user}
               element={chosenUser}
               channel={chosenChannel}
