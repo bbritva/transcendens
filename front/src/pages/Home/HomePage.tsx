d824c3bd7f521783af50253124bcfdbfe536c32b
Iaroslav NEVEROV <beeguy74@gmail.com>
2023-03-23 15:38:17 +0100

    login allert

--- front/src/pages/Home/HomePage.tsx
+++ front/src/pages/Home/HomePage.tsx
@@ -1,7 +1,30 @@
 import { Box, Typography, useTheme } from '@mui/material'
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import DialogSelect from 'src/components/DialogSelect/DialogSelect';
+import { selectLoggedIn } from 'src/store/authReducer';
 
 function HomePage() {
   const theme = useTheme();
+  const [loginText, setLoginText] = useState<string>('Login?');
+  const [openDialog, setOpenDialog] = useState<boolean>(false);
+  const isLogged = useSelector(selectLoggedIn);
+
+  useEffect(() => {
+    let counter = sessionStorage.getItem('counter');
+    if (!counter){
+      counter = '1';
+    } else {
+      if (counter.length > 1)
+        setLoginText("Login? Yes Login..... you know fish chips,  cup a tea .. bad food, worse weather ....Mary f%€king Poppins .... \nLogin ... ");
+      counter += '1';
+    }
+    if (!isLogged)
+      setTimeout(() => {setOpenDialog(true)}, 1200);
+    else
+      counter = '1'
+    sessionStorage.setItem('counter', counter);
+  }, [isLogged]);
 
   return (
     <Box
@@ -19,6 +42,21 @@ function HomePage() {
       <Typography variant="h2" color={theme.palette.secondary.main}>
         Pong game
       </Typography>
+      <DialogSelect
+        options={{}}
+        open={openDialog}
+        setOpen={setOpenDialog}
+      >        <Box
+          margin={"1rem"}
+          display={"flex"}
+          flexDirection={"column"}
+          alignItems={"center"}
+        >
+        <Typography variant="body1" maxWidth={"300px"}>
+          {loginText}
+        </Typography>
+        </Box>
+      </DialogSelect>
     </Box>
 
   );
